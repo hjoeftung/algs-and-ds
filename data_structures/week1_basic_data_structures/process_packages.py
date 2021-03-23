84ae5dfdf83f85b63b2b708a1da4423a11424cfb
hjoeftung <sto-ton@yandex.ru>
2021-03-23 11:49:56 +0300

    solve ds.week1.process_packages

--- data_structures/week1_basic_data_structures/process_packages.py
+++ data_structures/week1_basic_data_structures/process_packages.py
@@ -1,6 +1,5 @@
 # python3
-
-from collections import namedtuple
+from collections import namedtuple, deque
 
 Request = namedtuple("Request", ["arrived_at", "time_to_process"])
 Response = namedtuple("Response", ["was_dropped", "started_at"])
@@ -9,11 +8,20 @@ Response = namedtuple("Response", ["was_dropped", "started_at"])
 class Buffer:
     def __init__(self, size):
         self.size = size
-        self.finish_time = []
+        self.finish_time = deque(maxlen=self.size)
 
     def process(self, request):
-        # write your code here
-        return Response(False, -1)
+        while self.finish_time and self.finish_time[0] <= request.arrived_at:
+            self.finish_time.popleft()
+
+        if len(self.finish_time) == self.size:
+            return Response(False, -1)
+        elif len(self.finish_time) == 0:
+            self.finish_time.append(request.arrived_at + request.time_to_process)
+            return Response(False, request.arrived_at)
+        else:
+            self.finish_time.append(self.finish_time[-1] + request.time_to_process)
+            return Response(False, self.finish_time[-1] - request.time_to_process)
 
 
 def process_requests(requests, buffer):
@@ -26,7 +34,7 @@ def process_requests(requests, buffer):
 def main():
     buffer_size, n_requests = map(int, input().split())
     requests = []
-    for _ in range(n_requests):
+    for i in range(n_requests):
         arrived_at, time_to_process = map(int, input().split())
         requests.append(Request(arrived_at, time_to_process))
 
